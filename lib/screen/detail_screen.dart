931155efb106fb54606814cfa4d5043a812f8e6f
LeeSangHyun <leesang0204@gmail.com>
2020-07-03 17:14:00 +0900

    connect to firebase

--- lib/screen/detail_screen.dart
+++ lib/screen/detail_screen.dart
@@ -33,7 +33,7 @@ class _DetailScreenState extends State<DetailScreen> {
                     width: double.maxFinite,
                     decoration: BoxDecoration(
                       image: DecorationImage(
-                        image: AssetImage('images/' + widget.movie.poster),
+                        image: NetworkImage(widget.movie.poster),
                         fit: BoxFit.cover,
                       ),
                     ),
@@ -45,63 +45,61 @@ class _DetailScreenState extends State<DetailScreen> {
                           color: Colors.black.withOpacity(0.1),
                           child: Container(
                               child: Column(
-                                children: <Widget>[
-                                  Container(
-                                    padding: EdgeInsets.fromLTRB(0, 45, 0, 10),
-                                    child: Image.asset(
-                                        'images/' + widget.movie.poster),
-                                    height: 300,
+                            children: <Widget>[
+                              Container(
+                                padding: EdgeInsets.fromLTRB(0, 45, 0, 10),
+                                child: Image.network(widget.movie.poster),
+                                height: 300,
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(7),
+                                child: Text(
+                                  '99% 일치 2019 15+ 시즌 1개',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(fontSize: 13),
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(7),
+                                child: Text(
+                                  widget.movie.title,
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16),
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(3),
+                                child: FlatButton(
+                                  onPressed: () {},
+                                  color: Colors.red,
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Icon(Icons.play_arrow),
+                                      Text('재생'),
+                                    ],
                                   ),
-                                  Container(
-                                    padding: EdgeInsets.all(7),
-                                    child: Text(
-                                      '99% 일치 2019 15+ 시즌 1개',
-                                      textAlign: TextAlign.center,
-                                      style: TextStyle(fontSize: 13),
-                                    ),
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(5),
+                                child: Text(widget.movie.toString()),
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(5),
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  '출연: 현빈, 손예진, 서지혜\n 제작자: 이정효, 박지은',
+                                  style: TextStyle(
+                                    color: Colors.white60,
+                                    fontSize: 12,
                                   ),
-                                  Container(
-                                    padding: EdgeInsets.all(7),
-                                    child: Text(
-                                      widget.movie.title,
-                                      textAlign: TextAlign.center,
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 16),
-                                    ),
-                                  ),
-                                  Container(
-                                    padding: EdgeInsets.all(3),
-                                    child: FlatButton(
-                                      onPressed: () {},
-                                      color: Colors.red,
-                                      child: Row(
-                                        mainAxisAlignment: MainAxisAlignment
-                                            .center,
-                                        children: <Widget>[
-                                          Icon(Icons.play_arrow),
-                                          Text('재생'),
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                  Container(
-                                    padding: EdgeInsets.all(5),
-                                    child: Text(widget.movie.toString()),
-                                  ),
-                                  Container(
-                                    padding: EdgeInsets.all(5),
-                                    alignment: Alignment.centerLeft,
-                                    child: Text(
-                                      '출연: 현빈, 손예진, 서지혜\n 제작자: 이정효, 박지은',
-                                      style: TextStyle(
-                                        color: Colors.white60,
-                                        fontSize: 12,
-                                      ),
-                                    ),
-                                  )
-                                ],
-                              )),
+                                ),
+                              )
+                            ],
+                          )),
                         ),
                       ),
                     ),
@@ -122,7 +120,12 @@ class _DetailScreenState extends State<DetailScreen> {
                     Container(
                       padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
                       child: InkWell(
-                        onTap: () {},
+                        onTap: () {
+                          setState(() {
+                            like = !like;
+                            widget.movie.reference.updateData({'like': like});
+                          });
+                        },
                         child: Column(
                           children: <Widget>[
                             like ? Icon(Icons.check) : Icon(Icons.add),
@@ -146,14 +149,11 @@ class _DetailScreenState extends State<DetailScreen> {
                         child: Column(
                           children: <Widget>[
                             Icon(Icons.thumb_up),
-                            Padding(
-                                padding: EdgeInsets.all(5)
-                            ),
+                            Padding(padding: EdgeInsets.all(5)),
                             Text(
                               '평가',
                               style: TextStyle(
-                                  fontSize: 11, color: Colors.white60
-                              ),
+                                  fontSize: 11, color: Colors.white60),
                             )
                           ],
                         ),
@@ -165,14 +165,11 @@ class _DetailScreenState extends State<DetailScreen> {
                         child: Column(
                           children: <Widget>[
                             Icon(Icons.send),
-                            Padding(
-                                padding: EdgeInsets.all(5)
-                            ),
+                            Padding(padding: EdgeInsets.all(5)),
                             Text(
                               '공유',
                               style: TextStyle(
-                                  fontSize: 11, color: Colors.white60
-                              ),
+                                  fontSize: 11, color: Colors.white60),
                             )
                           ],
                         ),
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:netflix_clone/model/model_movie.dart';
 import 'package:netflix_clone/widget/box_slider.dart';
@@ -9,52 +10,27 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<Movie> movies = [
-    Movie.fromMap({
-      'title': '사랑의 불시착',
-      'keyword': '사랑/로맨스/판타지',
-      'poster': 'test_movie_1.png',
-      'like': false
-    }),
-    Movie.fromMap({
-      'title': '사랑의 불시착1',
-      'keyword': '사랑/로맨스/판타지',
-      'poster': 'test_movie_1.png',
-      'like': false
-    }),
-    Movie.fromMap({
-      'title': '사랑의 불시착2',
-      'keyword': '사랑/로맨스/판타지',
-      'poster': 'test_movie_1.png',
-      'like': false
-    }),
-    Movie.fromMap({
-      'title': '사랑의 불시착3',
-      'keyword': '사랑/로맨스/판타지',
-      'poster': 'test_movie_1.png',
-      'like': false
-    }),
-    Movie.fromMap({
-      'title': '사랑의 불시착3',
-      'keyword': '사랑/로맨스/판타지',
-      'poster': 'test_movie_1.png',
-      'like': false
-    }),
-    Movie.fromMap({
-      'title': '사랑의 불시착3',
-      'keyword': '사랑/로맨스/판타지',
-      'poster': 'test_movie_1.png',
-      'like': false
-    })
-  ];
+  Firestore firestore = Firestore.instance;
+  Stream<QuerySnapshot> streamData;
 
   @override
   void initState() {
     super.initState();
+    streamData = firestore.collection('movie').snapshots();
   }
 
-  @override
-  Widget build(BuildContext context) {
+  Widget _fetchData(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('movie').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return LinearProgressIndicator();
+        return _buildBody(context, snapshot.data.documents);
+      },
+    );
+  }
+
+  Widget _buildBody(BuildContext context, List<DocumentSnapshot> snapshot) {
+    List<Movie> movies = snapshot.map((d) => Movie.fromSnapshot(d)).toList();
     return ListView(
       children: <Widget>[
         Stack(
@@ -68,6 +44,14 @@ class _HomeScreenState extends State<HomeScreen> {
       ],
     );
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return _fetchData(context);
+  }
+
+
+
 }
 
 class TopBar extends StatelessWidget {
